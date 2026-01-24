f6315342433924e5a8ad05abb79ee22071a0499d
Hitesh <demo@gmail.com>
2026-01-24 12:28:02 +0530

    mod() added

--- src/main/java/sl/calc/MyCalc.java
+++ src/main/java/sl/calc/MyCalc.java
@@ -17,6 +17,10 @@ public class MyCalc {
 	{
 		return a/b;
 	}
+	public int mod(int a, int b)
+	{
+		return a%b;
+	}
 
 	public static void main(String[] args) {
 		MyCalc calc = new MyCalc();
@@ -24,6 +28,7 @@ public class MyCalc {
 		System.out.println("Diff is "+ calc.diff(20, 10));
 		System.out.println("Mul is "+ calc.mul(20, 10));
 		System.out.println("Div is "+ calc.div(20, 10));
+		System.out.println("Mod is "+ calc.mod(20, 10));
 
 	}
 
