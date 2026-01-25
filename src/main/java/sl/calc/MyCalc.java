7abe2be963032a9182e3b3fd58e4fb9421a78c2f
Dr. Hitesh Kumar Sharma <hkshitesh@gmail.com>
2026-01-25 09:44:21 +0530

    Update greeting message in show method

--- src/main/java/sl/calc/MyCalc.java
+++ src/main/java/sl/calc/MyCalc.java
@@ -23,7 +23,7 @@ public class MyCalc {
 	}
 	public void show()
 	{
-		System.out.println("Hello Jenkins. Welcome to DevOps Class");
+		System.out.println("Hello Jenkins. Welcome to DevOps Class !");
 	}
 
 	public static void main(String[] args) {
