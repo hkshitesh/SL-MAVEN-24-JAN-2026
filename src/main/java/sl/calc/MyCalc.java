d60b9d2713f35b0fb6e5ecb7e56287341583619b
Hitesh <demo@gmail.com>
2026-01-25 12:31:35 +0530

    Dev Branch Commit

--- src/main/java/sl/calc/MyCalc.java
+++ src/main/java/sl/calc/MyCalc.java
@@ -27,7 +27,7 @@ public class MyCalc {
 	}
 	public void greeting()
 	{
-		System.out.println("Hello Jenkins from Master Branch");
+		System.out.println("Hello Jenkins from Dev Branch");
 	}
 
 	public static void main(String[] args) {
